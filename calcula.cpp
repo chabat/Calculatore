419d3624e436dd1f890194eb4d08a969e45f3a4b
chabat <felipechabat@gmail.com>
2018-10-27 20:31:25 -0200

    Adicionado soma de dois numeros portanto que ambos sejam 1

--- calcula.cpp
+++ calcula.cpp
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
-  char num1[10], num2[10], op[10];
+  char num1[10], num2[10], op[10], ans[10];
 
   scanf("%s %s %s", num1, op, num2);
 
+  strcpy(ans, "nao sei bisho");
+  if(!strcmp(op, "mais")){
+    if(!strcmp(num1, "um")){
+      if(!strcmp(num2, "um")){
+	strcpy(ans, "dois");
+      }
+    }
+  }
+  printf("Resposta: %s\n", ans);
+  
   return 0;
 }
